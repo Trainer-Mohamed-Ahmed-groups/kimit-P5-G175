e7605dc06feb0161fb5f08ab8274dd7820cd6db8
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-02-12 21:46:00 +0200

    To relational operators

--- js/session.js
+++ js/session.js
@@ -1,93 +1,123 @@
-//*******************  Object *********************** */
-// var object = {
-//     key: value
-// }
+var person = {
+    name: "Ali",
+    age: 30,
+    gender: "male",
+    calcAgeInDays: function () {
+        return person.age * 365;
+    }
+}
 
-// Example =>
+console.log(person.age)
+console.log(person.calcAgeInDays())
 
-var car = {
-    name: 'Mercedes',
-    model: 2020,
-    isSold: true
+function sayHello(name = "Not person") {
+    console.log("Hello " + name)
+}
+
+sayHello("Mohamed")
+sayHello("Ali")
+sayHello()
+
+function calcAge(x) { return 2024 - x }
+
+
+function calcDays(x) { return 365 * x }
+
+// var username = prompt("Please enter your name "),
+//     userBirth = +prompt("Please enter your age"),
+//     age = calcAge(userBirth),
+//     ageInDays = calcDays(age);
+
+
+// console.log(username)
+// console.log(userBirth)
+// console.log(age)
+// console.log("Hello " + username + " your age is " + age + " and your age in days is " + ageInDays)
+
+var x = 10;
+
+if (x > 0) {
+    console.log("Positive")
+    if (5 > 0) { console.log("OK") }
+}
+else if (x < 0) {
+    console.log("Negative")
+}
+else {
+    console.log("ZERO")
+}
+console.log("********************")
+
+
+if (x = 5) {
+    console.log("I am five")
+}
+else {
+    console.log("Not five")
 }
 
 
-console.log(car)
-console.log(car['name'])
+var y = 5;
+
+console.log(++y)
+
+console.log(--y)
+
+console.log(y)
+
+// Rana    6 4 5
+// Bavley  undefined undefined 5
+// Mina    6 5 5
+// Ibrahim 6 5 5 
+// Esraa   6 4 5
+// Mazen   6 5 5 
+
+// Mohamed  5 6 5
+
+var z = 10;
+
+console.log(z++)
+console.log(z)
+console.log(--z)
+console.log(--z)
+console.log(z--)
+console.log(z++)
+console.log(++z)
+
+// Esraa    10 11 10 9 9 8 10
+// Mina     10 11 10 9 9 9 10
+// Rana     10 11 10 9 9 8 10
+// Mazen    10 11 10 9 9 9 10
+// Ibrahim  10 11 10 9 9 8 10
+// Bavley   10 11 10 9 9 8 10
+//          10 11 10 9 9 8 10 
+
+console.log(12 % 3)
 
 
-// Dot notation 
 
-console.log(car.name + " and it's model is " + car.model)
-
-// Bracket notation
-console.log(car['name'] + " and it's model is " + car['model'])
-
-//*******************  function *********************** */
+console.log(15 % 2)
 
 
-function greeting() {
-    console.log("Hello")
+var t = "5";
+
+if (t !== 5) {
+    console.log("OK")
+}
+else {
+    console.log("NO")
 }
 
-greeting()
+var userInput = +prompt("Please enter your number")
 
-// Function with parameter
-
-// Void function
-function greeting2(x) {
-    console.log("Hello " + x)
+if (userInput % 2 === 0) {
+    console.log("This is even number")
+}
+else {
+    console.log("This is odd number")
 }
 
-greeting2("Mohamed")
-greeting2("Ibrahim")
-greeting2("Mina")
+var testOne = +prompt("Enter one")
+var testTwo = +prompt("Enter one")
 
-// Function With parameters
-function sum(a, b) {
-    return (a + b)
-}
-
-function multi(a, b) {
-    return (a * b)
-}
-
-function sub(a, b) {
-    return (a - b)
-}
-
-function division(a, b) {
-    return (a / b)
-}
-
-// var num_one = +prompt("Please enter number one"),
-//     num_two = +prompt("Please enter number two");
-
-
-
-// console.log(sum(num_one, num_two))
-// console.log(sub(num_one, num_two))
-// console.log(multi(num_one, num_two))
-// console.log(division(num_one, num_two))
-
-// Returning value function
-function addNum() {
-    return 1 + 1
-}
-
-console.log(addNum())
-
-
-var persons = ["Mohamed", "Mina", "Ibrahim", "Ali", "Mazen",["test","test2"]];
-
-console.log(persons)
-console.log(persons.length)
-console.log(persons[2])
-console.log(persons[persons.length - 1])
-console.log(persons[60]) //undefined
-console.log(persons[5][0])
-
-persons[6] = "Esraa"
-console.log(persons)
-console.log(persons[6])
-console.log(persons.at(-1))
+console.log(testOne % testTwo)
